cf450609bc4fabbbe9af365b4038e7df59bd1e95
Pawan Kumar <pk89844@gmail.com>
2024-06-24 14:27:12 +0530

    DB functions

--- lib/api/api.dart
+++ lib/api/api.dart
@@ -2,6 +2,7 @@ import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
 
 class API {
+  final _header = {'content-type': 'application/json'};
   Response rootHandler(Request req) {
     return Response.ok('Hello, World!\n');
   }
@@ -9,5 +10,9 @@ class API {
   Response echoHandler(Request request) {
     final message = request.params['message'];
     return Response.ok('$message\n');
+  }
+
+  Response getSongs(Request request) {
+    return Response.ok('body', headers: _header);
   }
 }
--- lib/routes/app_router.dart
+++ lib/routes/app_router.dart
@@ -2,6 +2,11 @@ import 'package:musci_player_backend/api/api.dart';
 import 'package:musci_player_backend/routes/app_routes.dart';
 import 'package:shelf_router/shelf_router.dart';
 
+// ************************************************************************************************************************
+/// [AppRouter] holds APIs available throughout the server.
+/// The [AppRouter] connects the server to the Router and calls the associated API.
+/// Currently, [AppRouter] holds one static function [api] which is the source of connecting API functions to the Router.
+// ************************************************************************************************************************
 class AppRouter {
   static final API _api = API();
   static final Router _router = Router();
